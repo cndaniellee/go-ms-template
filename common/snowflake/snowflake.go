79e46f08edab0327f6a4fe9d41371157ce7fccf6
CNDanielLee <768318841@qq.com>
2023-04-21 15:25:31 +0800

    SnowFlake

---
+++ common/snowflake/snowflake.go
@@ -0,0 +1,178 @@
+package snowflake
+
+import (
+	"errors"
+	"fmt"
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"go.uber.org/atomic"
+	"sort"
+	"strconv"
+	"time"
+)
+
+const (
+	// KeepAlivePeriod 存活更新时间
+	KeepAlivePeriod = time.Second * 10
+	// GracePeriod 检查宽限时间
+	GracePeriod = time.Second * 5
+)
+
+type SnowFlake struct {
+	rds  *redis.Redis
+	hKey string
+
+	epoch     int64 // 起始时间戳
+	timestamp int64 // 当前时间戳，毫秒
+	serviceId int64 // 数据中心机房ID
+	workerId  int64 // 机器ID
+
+	timestampBits  int64 // 时间戳占用位数
+	serviceIdBits  int64 // 数据中心id所占位数
+	workerIdBits   int64 // 机器id所占位数
+	sequenceBits   int64 // 序列所占的位数
+	sequenceMask   int64 // 生成序列的掩码最大值
+	workerIdShift  int64 // 机器id左移偏移量
+	centerIdShift  int64 // 数据中心机房id左移偏移量
+	timestampShift int64 // 时间戳左移偏移量
+	maxTimeStamp   int64 // 最大支持的时间
+
+	sequence      *atomic.Int64 // 毫秒内序列号
+	lastTimestamp *atomic.Int64 // 上一次生成ID的时间戳
+}
+
+func New(rds *redis.Redis, serviceId int64) (*SnowFlake, error) {
+
+	var workerId int64
+
+	// Redis中以Map存储当前服务下的已使用ID+过期时间
+	hKey := fmt.Sprintf("snowflake:%d", serviceId)
+	all, err := rds.Hgetall(hKey)
+	if err != nil {
+		return nil, err
+	}
+	if len(all) > 0 {
+		// 获取所有有效的ID
+		occupiedIds := make([]int64, 0, len(all))
+		now := time.Now()
+		for k, v := range all {
+			var parse int64
+			// 解析租约
+			parse, err = strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			// 此ID的租约已到期
+			if time.UnixMilli(parse).Before(now) {
+				if _, err = rds.Hdel(hKey, k); err != nil {
+					return nil, err
+				}
+			} else {
+				// 解析ID
+				parse, err = strconv.ParseInt(k, 10, 64)
+				if err != nil {
+					return nil, err
+				}
+				occupiedIds = append(occupiedIds, parse)
+			}
+		}
+
+		if len(occupiedIds) > 0 {
+			// 将ID排序
+			sort.Sort(Int64Slice(occupiedIds))
+			// 找到可用的ID
+			var occupiedIndex int
+			for {
+				if workerId < occupiedIds[occupiedIndex] {
+					break
+				} else if occupiedIndex < len(occupiedIds) {
+					occupiedIndex++
+				} else {
+					break
+				}
+				workerId++
+			}
+		}
+	}
+
+	s := &SnowFlake{rds: rds, hKey: hKey}
+	s.epoch = int64(1672502400000) //设置起始时间戳：2023-01-01 00:00:00
+	s.serviceId = serviceId
+	s.workerId = workerId
+	s.serviceIdBits = 4  // 支持的最大无服务ID占位数，最大是15
+	s.workerIdBits = 6   // 支持的最大机器ID占位数，最大是63
+	s.timestampBits = 41 // 时间戳占用位数
+	s.maxTimeStamp = -1 ^ (-1 << s.timestampBits)
+
+	maxServiceId := -1 ^ (-1 << s.serviceIdBits)
+	maxWorkerId := -1 ^ (-1 << s.workerIdBits)
+
+	// 参数校验
+	if int(serviceId) > maxServiceId || serviceId < 0 {
+		return nil, errors.New(fmt.Sprintf("serviceId can't be greater than %d or less than 0", maxServiceId))
+	}
+	if int(workerId) > maxWorkerId || workerId < 0 {
+		return nil, errors.New(fmt.Sprintf("workerId can't be greater than %d or less than 0", maxWorkerId))
+	}
+
+	s.sequenceBits = 12                                                  // 序列在ID中占的位数（1毫秒中生成的），最大为4095
+	s.sequenceMask = -1 ^ (-1 << s.sequenceBits)                         // 计算毫秒内，最大的序列号
+	s.workerIdShift = s.sequenceBits                                     // 机器ID向左移12位
+	s.centerIdShift = s.sequenceBits + s.workerIdBits                    // 机房ID向左移18位
+	s.timestampShift = s.sequenceBits + s.workerIdBits + s.serviceIdBits // 时间截向左移22位
+
+	s.sequence = atomic.NewInt64(-1)
+	s.lastTimestamp = atomic.NewInt64(-1) // 上次生成 ID 的时间戳
+
+	// 开携程更新存活时间
+	go s.keepAlive()
+
+	return s, nil
+}
+
+func (s *SnowFlake) NextId() int64 {
+	now := time.Now().UnixMilli()     // 获取当前时间戳
+	if now < s.lastTimestamp.Load() { // 如果当前时间小于上一次 ID 生成的时间戳，说明发生时钟回拨
+		panic(fmt.Sprintf("clock moved backwards. Refusing to generate id for %d milliseconds", s.lastTimestamp.Load()-now))
+	}
+
+	t := now - s.epoch
+	if t > s.maxTimeStamp {
+		panic(fmt.Sprintf("epoch must be between 0 and %d", s.maxTimeStamp-1))
+	}
+
+	// 同一时间生成的，则序号+1
+	if s.lastTimestamp.Load() == now {
+		s.sequence.Store(s.sequence.Inc() & s.sequenceMask)
+		// 毫秒内序列溢出：超过最大值; 阻塞到下一个毫秒，获得新的时间戳
+		if s.sequence.Load() == 0 {
+			for now <= s.lastTimestamp.Load() {
+				now = time.Now().UnixMilli()
+			}
+		}
+	} else {
+		s.sequence.Store(0) // 时间戳改变，序列重置
+	}
+	// 保存本次的时间戳
+	s.lastTimestamp.Store(now)
+
+	// 根据偏移量，向左位移达到
+	return (t << s.timestampShift) | (s.serviceId << s.centerIdShift) | (s.workerId << s.workerIdShift) | s.sequence.Load()
+}
+
+func (s *SnowFlake) keepAlive() {
+	for {
+		if err := s.rds.Hset(s.hKey, strconv.FormatInt(s.workerId, 10), strconv.FormatInt(time.Now().Add(KeepAlivePeriod+GracePeriod).UnixMilli(), 10)); err != nil {
+			logx.Error("snowflake keep alive failed", err)
+			return
+		}
+		time.Sleep(KeepAlivePeriod)
+	}
+}
+
+// Int64Slice ID排序
+type Int64Slice []int64
+
+func (p Int64Slice) Len() int           { return len(p) }
+func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
